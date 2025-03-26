4390bbcf955e8e1eea904b29aec0ae330defe775
João Gabriel <joao.gabriel.xp2011@gmail.com>
2025-03-26 18:21:19 -0300

    second commit

--- game.js
+++ game.js
@@ -26,29 +26,44 @@ function preload() {
         frameHeight: 400,
     })
     this.load.spritesheet("enemy", "newenemy.png", {
-        frameWidth: 160,
-        frameHeight: 128,
+        frameWidth: 640,
+        frameHeight: 640,
     })
-
+    this.load.image("floor","floor.png");
 }
 function create() {
+    this.cameras.main.setBackgroundColor("#29757C");
+
     this.anims.create({
         key:"playerAnim",
         frames:this.anims.generateFrameNumbers("player", {start:0,end:3}),
         frameRate: 5,
         repeat:-1,
     }); 
-    player = this.add.sprite(400,300, "player").play("playerAnim");
+    player = this.physics.add.sprite(400,300, "player").play("playerAnim");
     player.setScale(0.4);
+    player.setCollideWorldBounds(true);
+    player.body.setGravity(0,0);
+    player.body.setAllowGravity(false);
+    /*player.body.immovable=true;
+    player.body.moves=false;*/
+    //Usar setVelocity para colisões sem interferir no movimento. Fazer teste!
     this.anims.create({
         key:"enemyAnim",
         frames:this.anims.generateFrameNumbers("enemy", {start:0, end:18}),
         frameRate:9,
         repeat:-1,
     });
-    enemy = this.add.sprite(700, 300, "enemy").play("enemyAnim");
-    
-    cursors=this.input.keyboard.createCursorsKeys();
+    enemy = this.physics.add.sprite(700, 300, "enemy").play("enemyAnim");
+    enemy.setScale(0.5);
+
+    const floor = this.physics.add.staticGroup();
+    floor.create(730, 500, "floor").setScale(1.5).refreshBody();
+
+    this.physics.add.collider(player, floor);
+    this.physics.add.collider(enemy,floor);
+
+    cursors=this.input.keyboard.createCursorKeys();
     
 }
 function update() {
